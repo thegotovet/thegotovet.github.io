57ae3ebd5450ba112cbbb9e8aebac533013e858f
somto amaugo <amaugosomto@gmail.com>
2019-12-11 16:32:59 +0100

    build

---
+++ assets/js/custom.js
@@ -0,0 +1,45 @@
+(function ($) {
+  "use strict";
+  // menu fixed js code
+  $(window).scroll(function () {
+    var window_top = $(window).scrollTop() + 1;
+    if (window_top > 50) {
+      $('.main_menu').addClass('menu_fixed animated fadeInDown');
+    } else {
+      $('.main_menu').removeClass('menu_fixed animated fadeInDown');
+    }
+  });
+
+// Search Toggle
+  $("#search_input_box").hide();
+  $("#search_1").on("click", function () {
+    $("#search_input_box").slideToggle();
+    $("#search_input").focus();
+  });
+  $("#close_search").on("click", function () {
+    $('#search_input_box').slideUp(500);
+  });
+
+//------- Mailchimp js --------//  
+  // function mailChimp() {
+  //   $('#mc_embed_signup').find('form').ajaxChimp();
+  // }
+  // mailChimp();
+
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight){
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      } 
+    });
+  }
+
+
+}(jQuery));
